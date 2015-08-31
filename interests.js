3712ab5207d400ddf30570c544752a75192f45fe
Timmy Siauw <Timmy@airbears2-10-142-174-172.airbears2.1918.berkeley.edu>
2015-08-31 15:59:17 -0700

    lots of changes, mostly to interests.html

---
+++ interests.js
@@ -0,0 +1,434 @@
+var interests = [
+	{
+		name: "Mikey Gee",
+		subtitle: "Best Friend",
+		link: "http://mikeygee.com/",
+		category: ["People", "Friends"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Trucy Phan",
+		subtitle: null,
+		link: "http://trucyphan.com/",
+		category: ["People", "Friends"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Leah Serb",
+		subtitle: null,
+		link: "http://leahserb.wix.com/fossi-baht",
+		category: ["People", "Friends"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Maya Abu-Mansour",
+		subtitle: null,
+		link: "http://www.fitnessbymaya.com/",
+		category: ["People", "Friends"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Mark Jellison",
+		subtitle: null,
+		link: "http://markjellison.com/",
+		category: ["People"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Sean Darling-Hammond",
+		subtitle: null,
+		link: "https://www.facebook.com/thegivingninja",
+		category: ["People", "Friends"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Sean Plott",
+		subtitle: null,
+		link: "http://day9.tv/",
+		category: ["People"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Dan Carlin\'s Hardcore History",
+		subtitle: null,
+		link: "",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Adriene",
+		subtitle: null,
+		link: "https://www.youtube.com/user/yogawithadriene",
+		category: ["People"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "node.js",
+		subtitle: null,
+		link: "https://nodejs.org/",
+		category: ["Javascript"],
+		keywords: ["javascript", "programming", "web"],
+		blurb: null
+	},
+	{
+		name: "socket.io",
+		subtitle: null,
+		link: "http://socket.io/",
+		category: ["Javascript"],
+		keywords: ["javascript", "programming", "web"],
+		blurb: null
+	},
+	{
+		name: "D3.js",
+		subtitle: null,
+		link: "http://d3js.org/",
+		category: ["Javascript"],
+		keywords: ["javascript", "programming", "web"],
+		blurb: null
+	},
+	{
+		name: "three.js",
+		subtitle: null,
+		link: "http://threejs.org/",
+		category: ["Javascript"],
+		keywords: ["javascript", "programming", "web"],
+		blurb: null
+	},
+	{
+		name: "paper.js",
+		subtitle: null,
+		link: "http://paperjs.org/",
+		category: ["Javascript"],
+		keywords: ["javascript", "programming", "web"],
+		blurb: null
+	},
+	{
+		name: "Backbone.js",
+		subtitle: null,
+		link: "http://backbonejs.org/",
+		category: ["Javascript"],
+		keywords: ["javascript", "programming", "web"],
+		blurb: null
+	},
+	{
+		name: "async.js",
+		subtitle: null,
+		link: "https://github.com/caolan/async",
+		category: ["Javascript"],
+		keywords: ["javascript", "programming", "web"],
+		blurb: null
+	},
+	{
+		name: "jsdicom",
+		subtitle: null,
+		link: "https://github.com/Infogosoft/jsdicom",
+		category: ["Javascript"],
+		keywords: ["javascript", "programming", "web"],
+		blurb: null
+	},
+	{
+		name: "The Four Hour Work Week",
+		subtitle: null,
+		link: "http://www.amazon.com/The-4-Hour-Workweek-Anywhere-Expanded/dp/0307465357",
+		category: ["Book"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "The Four Hour Body",
+		subtitle: null,
+		link: "http://www.amazon.com/Hour-Body-Uncommon-Incredible-Superhuman/dp/030746363X/ref=sr_1_1?s=books&ie=UTF8&qid=1441043867&sr=1-1&keywords=the+four+hour+body",
+		category: ["Book"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "The Calorie Myth",
+		subtitle: null,
+		link: "http://www.amazon.com/Calorie-Myth-Exercise-Weight-Better/dp/0062267345/ref=sr_1_1?s=books&ie=UTF8&qid=1441043887&sr=1-1&keywords=the+calorie+myth",
+		category: ["Book"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Your Body's Many Cries for Water",
+		subtitle: null,
+		link: "http://www.amazon.com/Your-Bodys-Many-Cries-Water/dp/0970245882/ref=sr_1_1?s=books&ie=UTF8&qid=1441043906&sr=1-1&keywords=your+body%27s+many+cries+for+water",
+		category: ["Book"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Algorithms",
+		subtitle: null,
+		link: "http://beust.com/algorithms.pdf",
+		category: ["Book"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Convex Optimization",
+		subtitle: null,
+		link: "https://web.stanford.edu/~boyd/cvxbook/bv_cvxbook.pdf",
+		category: ["Book"],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Berkeley Ironworks",
+		subtitle: null,
+		link: "https://touchstoneclimbing.com/ironworks/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Touchstone Climbing",
+		subtitle: null,
+		link: "https://touchstoneclimbing.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Total Athletic Conditioning",
+		subtitle: null,
+		link: "http://recsports.berkeley.edu/fitness-wellness/group-exercise/all-around-workouts/total-athletic-conditioning/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "SURF",
+		subtitle: null,
+		link: "http://surf.berkeley.edu/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Matlab",
+		subtitle: null,
+		link: "http://www.mathworks.com/products/matlab/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Python",
+		subtitle: null,
+		link: "https://www.python.org/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Midori",
+		subtitle: null,
+		link: "http://www.midorisushi.net/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "IMDB",
+		subtitle: null,
+		link: "http://www.imdb.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Go",
+		subtitle: null,
+		link: "https://en.wikipedia.org/wiki/Go_(game)",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "GoPanda2",
+		subtitle: null,
+		link: "http://pandanet-igs.com/communities/gopanda2",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	/*{
+		name: "Round-Off Error",
+		subtitle: null,
+		link: "",
+		category: [],
+		keywords: [],
+		blurb: null
+	},*/
+	{
+		name: "IFSC",
+		subtitle: null,
+		link: "http://www.ifsc-climbing.org/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "LouderThan11",
+		subtitle: null,
+		link: "https://lt11.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "LouderThan11 (youtube)",
+		subtitle: null,
+		link: "https://www.youtube.com/user/LouderThan11",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Starcraft",
+		subtitle: null,
+		link: "http://us.battle.net/sc2/en/?-",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "TeamLiquid.net",
+		subtitle: null,
+		link: "http://www.teamliquid.net/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Github",
+		subtitle: null,
+		link: "https://github.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "League of Legends",
+		subtitle: null,
+		link: "http://na.leagueoflegends.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Straw",
+		subtitle: null,
+		link: "http://www.strawsf.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Sushi House",
+		subtitle: null,
+		link: "http://www.e-sushihouse.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Salary Negotiation",
+		subtitle: null,
+		link: "http://www.kalzumeus.com/2012/01/23/salary-negotiation/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Latex",
+		subtitle: null,
+		link: "http://www.latex-project.org/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "LatexIt",
+		subtitle: null,
+		link: "http://www.chachatelier.fr/latexit/latexit-downloads.php?lang=en",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "La Mission",
+		subtitle: null,
+		link: "http://www.yelp.com/biz/la-mission-berkeley",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "La Burrita",
+		subtitle: null,
+		link: "http://www.yelp.com/biz/la-burrita-berkeley",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Hero Machine",
+		subtitle: null,
+		link: "http://www.heromachine.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Goorin Bros.",
+		subtitle: null,
+		link: "http://www.goorin.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Indochino",
+		subtitle: null,
+		link: "http://www.indochino.com/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Bungee Adventures",
+		subtitle: null,
+		link: "http://bungeeadventures.net/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Google Material Design",
+		subtitle: null,
+		link: "https://www.google.com/design/spec/material-design/introduction.html",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+	{
+		name: "Startup Podcaset",
+		subtitle: null,
+		link: "https://gimletmedia.com/show/startup/",
+		category: [],
+		keywords: [],
+		blurb: null
+	},
+];
